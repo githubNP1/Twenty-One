e4bd68cb8cfa78290ee191ef53f9fef02a3785f9
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-19 10:32:31 +0100

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,5 +1,15 @@
 package Twenty_One;
 
 public class Card {
-    
+    String number;
+    String suit;
+    int value;
+    Boolean faceUp;
+
+    public Card(String number, String suit, int value, Boolean faceUp){
+        this.number = number;
+        this.suit = suit;
+        this.value = value;
+        this.faceUp = faceUp;
+    }
 }
