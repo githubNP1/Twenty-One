3fc11042c51d80a85ae4d962bb3184f46a755d59
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-19 11:25:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
 package Twenty_One;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    ArrayList<Card> deck = new ArrayList<>();
     
+    public static void main() throws IOException{
+        Main game = new Main();
+        Player player = new Player(10000);
+        Dealer dealer = new Dealer();
+        game.getDeck();
+        game.run();
+    }
+
+    public void run(){
+        while(deck.size() > 30){
+            
+        }
+    }
+    
+    
+    
+    public void getDeck() throws FileNotFoundException, IOException{  //shuffles 6 decks of cards to be used
+        for(int i = 0; i < 6; i++){
+            BufferedReader reader = new BufferedReader(new FileReader("Deck.txt"));
+            for (int j = 0; j < 52; j++){                                                          
+                String[] line = reader.readLine().split("[:]");                                                
+                Card one = new Card(line[0], line[1], Integer.valueOf(line[2]));                                     
+                deck.add(one);
+            }
+            reader.close();
+        }
+        Collections.shuffle(deck); 
+    }
 }
