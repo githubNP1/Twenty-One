dd5a7360e763772bfff2804f39fc2fe3dc2fb64e
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-19 10:38:10 +0100

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,5 +1,18 @@
 package Twenty_One;
 
+import java.util.*;
+
 public class Hand {
+    ArrayList<Card> hand = new ArrayList<>();
+    Boolean bust = false;
+    int score;
+    int bet;
     
+    public int getTotalScore(){
+        score = 0;
+        for (Card card : hand){
+            score += card.value;
+        }
+        return score;
+    }
 }
