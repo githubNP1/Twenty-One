d6c46a32bcc167f0b7761aa3694c899bdaab8651
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-19 10:45:13 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,12 @@
 package Twenty_One;
 
+import java.util.*;
+
 public class Player {
+    int chips;  //this is decided at beginning
+    ArrayList<Hand> hands = new ArrayList<>();
     
+    public Player(int chips){
+        this.chips = chips;
+    }
 }
