30364a133fda3e63ba314fd694cd315470c78588
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-19 17:05:44 +0100

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Hand {
     ArrayList<Card> hand = new ArrayList<>();
     Boolean bust = false;
+    Boolean natural = false;
     int score;
     int bet;
     
@@ -14,5 +15,10 @@ public class Hand {
             score += card.value;
         }
         return score;
+    }
+    
+    public void add(Card card, Boolean faceUp){
+        hand.add(card);
+        card.faceUp = faceUp;
     }
 }
