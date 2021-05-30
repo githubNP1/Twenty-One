68375c6d7206b8009ebe2ffd4d53c42aa25da623
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-05-30 15:59:07 +0100

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -31,20 +31,47 @@ public class Hand {
     }
     
     public Boolean checkNatural(){  //returns Boolean, maybe no need for Boolean natural
-        if(hand.size() == 2 && getTotalScore() == 21){
+        if(hand.get(0).ace && hand.get(1).ten || hand.get(0).ten && hand.get(1).ace){
             return true;
         }
+        //if(hand.size() == 2 && getTotalScore() == 21){
+        //    return true;
+        //}
         return false;
+    }
+    
+    public void checkBust(){
+        if(getTotalScore() > 21){
+            bust = true;
+        }
     }
     
     public void clear(){
         hand.clear();
     }
     
+    public Boolean checkIfDoubleable(){
+        if(!doubled && hand.size() == 2 && getTotalScore() >= 9 && getTotalScore() <= 11){
+            return true;
+        }
+        return false;
+    }
+    
     public String makeMove(){
         String moves = "Do you want to 1) stand, 2) hit";
+        checkIfDoubleable();
         if(!doubled){moves += ", 3) double";}
         if(splittable){moves += " or 4) split";}
         return moves;
+    }
+    
+    public ArrayList moveChoices(){
+        ArrayList<String> choices = new ArrayList<>();
+        choices.add("stand");
+        choices.add("hit"); 
+        checkIfDoubleable();
+        if(!doubled){choices.add("double");}
+        if(splittable){choices.add("split");} 
+        return choices;
     }
 }
