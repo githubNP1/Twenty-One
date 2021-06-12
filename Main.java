a2557e4dbdb59fd82857a53eef561b985e140339
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-06-12 10:46:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,20 +13,21 @@ public class Main extends JPanel implements ActionListener{
     ArrayList<Card> fullDeck = new ArrayList<>();
     BufferedImage cards;
     BufferedImage table;
-    //int[] card;
+    
     ArrayList<Card> dealerCards = new ArrayList<>();
     ArrayList<Card> playerFirstCards = new ArrayList<>();
     ArrayList<Card> playerSecondCards = new ArrayList<>();
     
-    JButton hit, stand, Double, split, Bet, Chip1, Chip5, Chip10, Chip25, Chip50, Chip100, Chip500, Chip1000;
+    JButton hit, stand, Double, split, Bet, Chip1, Chip5, Chip10, Chip25, Chip50, Chip100, Chip500, Chip1000, yes, no;
     JTextArea display;
-    JTextField entry;
+    
     JLabel labelBet;
     JLabel labelChips;
-    JLabel chips;
     
     Boolean readyToBet = false;
+    int calculatedBet;
     int bet;
+    Boolean betMade = false;
     
     Boolean readyToMove = false;
     int hand;
@@ -36,19 +37,22 @@ public class Main extends JPanel implements ActionListener{
     
     ArrayList<JButton> moveButtons;
     ArrayList<JButton> chipButtons;
+    ArrayList<JButton> ynButtons;
+    
+    Boolean y, n;
     
     public Main() throws IOException{
         JFrame frame = new JFrame();
         frame.add(this);
         frame.setSize(1020, 750); 
-        setupScreen();
-        frame.setVisible(true);
         
         player = new Player(10000);
         dealer = new Dealer();
+        getCardImages();
+        setupScreen();
+        frame.setVisible(true);
         
         getDeck();
-        getCardImages();
         run(player, dealer);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,7 +61,6 @@ public class Main extends JPanel implements ActionListener{
     public static void main(String[] args) throws IOException{ 
     //play runs out when chips greater than 1 - ok, but if no chips left, can still double - will go into minus chips
         Main game = new Main();
-        
     }
     
     public void paintComponent(Graphics g){
@@ -66,17 +69,23 @@ public class Main extends JPanel implements ActionListener{
         try{
             int i = 0;
             for(Card card : dealerCards){
-                g.drawImage(cards, 400 + i*72, 70, 472 + i*72, 166 + i, card.x1, card.y1, card.x2, card.y2, null);
+                if(card.faceUp){
+                    g.drawImage(cards, 400 + i*72, 70, 472 + i*72, 166, card.x1, card.y1, card.x2, card.y2, null);
+                }
                 i++;
             }
             int j = 0;
             for(Card card : playerFirstCards){
-                g.drawImage(cards, 80 + i*72, 400, 152 + i*72, 496 + i, card.x1, card.y1, card.x2, card.y2, null);
+                if(card.faceUp){
+                    g.drawImage(cards, 80 + j*72, 400, 152 + j*72, 496, card.x1, card.y1, card.x2, card.y2, null);
+                }
                 j++;
             }
             int k = 0;
             for(Card card : playerSecondCards){
-                g.drawImage(cards, 400 + i*72, 400, 472 + i*72, 496 + i, card.x1, card.y1, card.x2, card.y2, null);
+                if(card.faceUp){
+                    g.drawImage(cards, 400 + k*72, 400, 472 + k*72, 496, card.x1, card.y1, card.x2, card.y2, null);
+                }
                 k++;
             }
         }catch(Exception e){}
@@ -90,55 +99,54 @@ public class Main extends JPanel implements ActionListener{
             table = ImageIO.read(input2);
         }
         catch (Exception e) {System.out.println(e);}
-        //card = new int[]{fullDeck.get(17).x1, fullDeck.get(17).y1, fullDeck.get(17).x2, fullDeck.get(17).y2};
-        //repaint();
     }
     
     public void setupScreen(){ //could be condensed?
         this.setLayout(null); 
         moveButtons = new ArrayList<>();
         chipButtons = new ArrayList<>();      
+        ynButtons = new ArrayList<>();      
         
-        hit = createButtons(hit, "hit.jpg", 80, 480, 70, 40, false, moveButtons);
-        stand = createButtons(stand, "stand.jpg", 200, 480, 137, 38, false, moveButtons);
-        Double = createButtons(Double, "double.jpg", 400, 480, 170, 40, false, moveButtons);
-        split = createButtons(split, "split.jpg", 600, 480, 114, 40, false, moveButtons);
-        Bet = createButtons(Bet, "bet.jpg", 120, 80, 132, 68, true, chipButtons);
-        Chip1 = createButtons(Chip1, "1 chip.jpg", 30, 570, 114, 114, true, chipButtons);
-        Chip5 = createButtons(Chip5, "5 chip.jpg", 150, 570, 113, 116, true, chipButtons);
-        Chip10 = createButtons(Chip10, "10 chip.jpg", 270, 570, 114, 116, true, chipButtons);
-        Chip25 = createButtons(Chip25, "25 chip.jpg", 390, 570, 113, 114, true, chipButtons);
-        Chip50 = createButtons(Chip50, "50 chip.jpg", 510, 570, 114, 114, true, chipButtons);
-        Chip100 = createButtons(Chip100, "100 chip.jpg", 630, 570, 114, 114, true, chipButtons);
-        Chip500 = createButtons(Chip500, "500 chip.jpg", 750, 570, 116, 116, true, chipButtons);
-        Chip1000 = createButtons(Chip1000, "1000 chip.jpg", 870, 570, 114, 116, true, chipButtons);
+        hit = createButtons(hit, "hit.jpg", 80, 570, 70, 40, moveButtons);
+        stand = createButtons(stand, "stand.jpg", 200, 570, 137, 38,  moveButtons);
+        Double = createButtons(Double, "double.jpg", 380, 570, 170, 40, moveButtons);
+        split = createButtons(split, "split.jpg", 600, 570, 114, 40, moveButtons);
+        yes = createButtons(yes, "yes.jpg", 300, 500, 80, 70, ynButtons);
+        no = createButtons(no, "no.jpg", 500, 500, 80, 70, ynButtons);
+        Bet = createButtons(Bet, "bet.jpg", 120, 80, 132, 68, chipButtons);
+        Chip1 = createButtons(Chip1, "1 chip.jpg", 30, 570, 114, 114, chipButtons);
+        Chip5 = createButtons(Chip5, "5 chip.jpg", 150, 570, 113, 116, chipButtons);
+        Chip10 = createButtons(Chip10, "10 chip.jpg", 270, 570, 114, 116, chipButtons);
+        Chip25 = createButtons(Chip25, "25 chip.jpg", 390, 570, 113, 114, chipButtons);
+        Chip50 = createButtons(Chip50, "50 chip.jpg", 510, 570, 114, 114, chipButtons);
+        Chip100 = createButtons(Chip100, "100 chip.jpg", 630, 570, 114, 114, chipButtons);
+        Chip500 = createButtons(Chip500, "500 chip.jpg", 750, 570, 116, 116, chipButtons);
+        Chip1000 = createButtons(Chip1000, "1000 chip.jpg", 870, 570, 114, 116, chipButtons);
         
-        labelBet = createLabels(labelBet, 880, 500, 60, 20);
-        labelChips = createLabels(labelChips, 880, 520, 100, 20);
+        labelBet = createLabels(labelBet, "BET: ", bet, 880, 520, 60, 20);
+        labelChips = createLabels(labelChips, "CHIPS: ", player.chips, 880, 540, 100, 20);
         
-        display = new JTextArea(5,10); display.setLineWrap(true); display.setWrapStyleWord(true);
-        display.setBounds(700, 200, 100, 50); 
-        entry = new JTextField(10);
-        entry.setBounds(700, 300, 50, 20);
+        display = new JTextArea(); 
+        display.setLineWrap(true); 
+        display.setWrapStyleWord(true);
+        display.setBounds(730, 470, 250, 50); 
         this.add(display); 
-        this.add(entry); 
         
-        //repaint(); //is this necessary
     }
     
-    public JButton createButtons(JButton button, String file, int x, int y, int width, int height, Boolean visible, ArrayList<JButton> group){ //creates buttons, uses file as imageicon, set bounds
+    public JButton createButtons(JButton button, String file, int x, int y, int width, int height, ArrayList<JButton> group){ //creates buttons, uses file as imageicon, set bounds
         ImageIcon image = new ImageIcon(file);
         button = new JButton(image);
         button.setBounds(x, y, width, height); 
-        button.setVisible(visible);
+        button.setVisible(false);
         group.add(button);
         this.add(button);
         button.addActionListener(this);
         return button;
     }
     
-    public JLabel createLabels(JLabel label, int x, int y, int width, int height){
-        label = new JLabel(); 
+    public JLabel createLabels(JLabel label, String text, int text2, int x, int y, int width, int height){
+        label = new JLabel(text + text2); 
         label.setBounds(x, y, width, height); 
         this.add(label);
         return label;
@@ -164,29 +172,47 @@ public class Main extends JPanel implements ActionListener{
         else if(e.getSource() == split){
             
         }
+        else if(e.getSource() == yes){
+            y = true;
+        }
+        else if(e.getSource() == no){
+            n = true;
+        }
+        else if (e.getSource() == Bet){
+            bet = calculatedBet;
+            calculatedBet = 0;
+        }
         else if (e.getSource() == Chip1){
-            bet += 1;
+            calculatedBet += 1;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip5){
-            bet += 5;
+            calculatedBet += 5;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip10){
-            bet += 10;
+            calculatedBet += 10;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip25){
-            bet += 25;
+            calculatedBet += 25;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip50){
-            bet += 50;
+            calculatedBet += 50;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip100){
-            bet += 100;
+            calculatedBet += 100;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip500){
-            bet += 500;
+            calculatedBet += 500;
+            labelBet.setText("BET: " + calculatedBet);
         }
         else if (e.getSource() == Chip1000){
-            bet += 1000;
+            calculatedBet += 1000;
+            labelBet.setText("BET: " + calculatedBet);
         }
     }
 
@@ -204,120 +230,103 @@ public class Main extends JPanel implements ActionListener{
         scan.close();
     }
     
-    public void editDisplay(String line){
-        display.setText(line);
-        repaint();
-    }
-    
     public void playerMakesBet(Scanner scan, Player player){
-        //System.out.println("Enter how much you would like to bet");
-        editDisplay("Enter how much you would like to bet");
-        readyToBet = true;
-        while (bet <= 0 || bet > player.chips){
-            if(bet <= 0){
-                editDisplay("You must enter a valid amount");
-            }
-            else if(bet > player.chips){
-                editDisplay("You do not have enough chips. Enter another amount");
-            }
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException ex) {}
-        }
-        readyToBet = false;
-        //int bet = scan.nextInt();
-        //while (bet <= 0 || bet > player.chips){
-            //System.out.println("You must enter a valid amount");
-            //bet = scan.nextInt();
-        //}
+        changeButtonVisibility(chipButtons, true);
+        display.setText("Enter how much you would like to bet \n You must enter a valid amount");
+        calculatedBet = 0;
+        do{
+            bet = 0;
+            try{Thread.sleep(200);} catch (InterruptedException ex) {}
+        } while(bet <= 0 || bet > player.chips);
+        display.setText(""); 
+        changeButtonVisibility(chipButtons, false);
         player.hands.clear();
         player.newPlayerHand().bet = bet;
-        //player.hand.bet = bet;
         player.chips -= bet;
-        System.out.println("Chips left: " + player.chips);
         changeButtonVisibility(chipButtons, false);
         labelBet.setText("BET: " + bet);    
-        labelChips.setText("Chips: " + player.chips);
+        labelChips.setText("CHIPS: " + player.chips);
     }
     
     public void initialDeal(Player player, Dealer dealer){
-        changeButtonVisibility(moveButtons, true);
         dealer.hand.clear();  //maybe put this in a seperate method
         dealer.hand.bust = false;
         player.natural = false;
         dealer.natural = false;
         player.getFirstHand().add(deck.get(0), true); 
-        //player.hands.get(0).add(deck.get(0), true); 
         dealer.hand.add(deck.get(1), true); 
         player.getFirstHand().add(deck.get(2), true);  
         dealer.hand.add(deck.get(3), false); 
         deck.removeIf(n -> (deck.indexOf(n) >= 0 && deck.indexOf(n) <= 3)); 
-        //System.out.println("You have a " + player.hand.hand.get(0).number + " of " + player.hand.hand.get(0).suit + " and a " + player.hand.hand.get(1).number + " of " + player.hand.hand.get(1).suit);
-        player.seeCards(); playerFirstCards = player.getFirstHand().hand; repaint();
-        if(player.getFirstHand().checkNatural()){player.natural = true; System.out.println("You have a natural");}
-        dealer.seeCards(); dealerCards = dealer.hand.hand; repaint();
-        //if(player.hand.hand.get(0).number.equals(player.hand.hand.get(1).number)){player.hand.splittable = true;}
-        //else{player.hand.splittable = false;}
+        playerFirstCards = player.getFirstHand().hand; 
+        repaint();
+        if(player.getFirstHand().checkNatural()){player.natural = true; display.setText("You have a natural");}
+        dealerCards = dealer.hand.hand; 
+        repaint();
         player.checkIfFirstHandCanBeSplit();
     }
     
     public void initialPlay(Scanner scan, Player player, Dealer dealer){
         //first see if dealers faceup card is ace, if so, player can make an insurance bet
         if(dealer.getFaceUpCard().ace){
-            System.out.println("Would you like to make an insurance bet?");
-            String choice = scan.next();
-            while (!"yes".equals(choice) && !"no".equals(choice)){
-                System.out.println("Enter 'yes' or 'no'");
-                choice = scan.next();
+            y = false;
+            n = false;
+            changeButtonVisibility(moveButtons, false);
+            changeButtonVisibility(ynButtons, true);
+            display.setText("Would you like to make an insurance bet?");
+            while(!y && !n){
+                try{Thread.sleep(200);} catch (InterruptedException ex) {}
             }
-            if(choice.equals("yes")){
-                System.out.println("Enter how much you would like to bet");
-                System.out.println("You can bet up to half your original bet");
-                player.insuranceBet = scan.nextInt();
-                while (player.insuranceBet <= 0 || player.insuranceBet > player.chips || player.insuranceBet > player.getFirstHand().bet / 2){
-                    System.out.println("You must enter a valid amount");
-                    player.insuranceBet = scan.nextInt();
-                }
+            display.setText(""); 
+            changeButtonVisibility(ynButtons, false);
+            if(y){
+                changeButtonVisibility(chipButtons, true);
+                display.setText("Enter how much you would like to bet /n You can bet up to half your original bet");
+                calculatedBet = 0;
+                do{
+                    bet = 0;
+                    try{Thread.sleep(200);} catch (InterruptedException ex) {}
+                } while(bet <= 0 || bet > player.getFirstHand().bet);
+                changeButtonVisibility(chipButtons, false);
+                player.insuranceBet = bet;
+                display.setText(""); 
                 player.chips -= player.insuranceBet;
                 player.insurance = true;
-                System.out.println("Chips left: " + player.chips);
-                labelChips.setText("Chips: " + player.chips);
+                labelChips.setText("CHIPS: " + player.chips);
             }
         }
         //if face up card is ace of ten card, dealer checks facedown card
         if(dealer.getFaceUpCard().ace || dealer.getFaceUpCard().ten){
-            System.out.println("The dealer checks the hole card"); 
+            display.setText("The dealer checks the hole card"); 
             if(dealer.hand.checkNatural()){
                 dealer.natural = true;
                 dealer.turnOverFaceDownCard();
-                System.out.println("The dealer has a natural");
+                repaint();
+                display.setText("The dealer has a natural");
                 if(player.insurance){
-                    System.out.println("You win twice your insurance bet");
+                    display.setText("You win twice your insurance bet");
                     player.chips += player.insuranceBet * 3;
-                    System.out.println("Chips left: " + player.chips);
-                    labelChips.setText("Chips: " + player.chips);
+                    labelChips.setText("CHIPS: " + player.chips);
                     player.insurance = false;
                 }
             }
             else{
-                System.out.println("The dealer does not have a natural");
+                display.setText("The dealer does not have a natural");
                 if(player.insurance){
-                    System.out.println("You lose your insurance bet");
+                    display.setText("You lose your insurance bet");
                     player.insurance = false;
                 }
             }
         }
         if(player.natural && dealer.natural){ //standoff
-            System.out.println("Standoff: Your bet is returned to you");
+            display.setText("Standoff: Your bet is returned to you");
             player.chips += player.getFirstHand().bet;
-            System.out.println("Chips left: " + player.chips);
-            labelChips.setText("Chips: " + player.chips);
+            labelChips.setText("CHIPS: " + player.chips);
         }
         else if(player.natural && !dealer.natural){ //player wins
-            System.out.println("You receive 1.5 times your bet in winnings");
+            display.setText("You receive 1.5 times your bet in winnings");
             player.chips += player.getFirstHand().bet * 2.5;
-            System.out.println("Chips left: " + player.chips);
-            labelChips.setText("Chips: " + player.chips);
+            labelChips.setText("CHIPS: " + player.chips);
         }
         else{
             play(player, dealer, scan);
@@ -403,7 +412,7 @@ public class Main extends JPanel implements ActionListener{
             System.out.println("Dealer is bust, you win");
             player.chips += 2 * playerHand.bet;
             System.out.println("Chips left: " + player.chips);
-            labelChips.setText("Chips: " + player.chips);
+            labelChips.setText("CHIPS: " + player.chips);
         }
         //settlement
         else if(dealer.hand.getTotalScore() > playerHand.getTotalScore()){
@@ -413,13 +422,13 @@ public class Main extends JPanel implements ActionListener{
             System.out.println("You win");
             player.chips += 2 * playerHand.bet;
             System.out.println("Chips left: " + player.chips);
-            labelChips.setText("Chips: " + player.chips);
+            labelChips.setText("CHIPS: " + player.chips);
         }
         else if(dealer.hand.getTotalScore() == playerHand.getTotalScore()){
             System.out.println("Standoff");
             player.chips += playerHand.bet;
             System.out.println("Chips left: " + player.chips);
-            labelChips.setText("Chips: " + player.chips);
+            labelChips.setText("CHIPS: " + player.chips);
         }
     }
     
